cab6d2e5b38db7e8757581300d29c3771a438458
FixerDiego <57854488+FixerDiegoB@users.noreply.github.com>
2023-05-08 20:47:29 -0500

    Update Computer.py

--- Proyecto-3S2/Sprint_4/Produccion/Computer.py
+++ Proyecto-3S2/Sprint_4/Produccion/Computer.py
@@ -3,25 +3,33 @@ import random
 
 class Computer:
     @staticmethod
-    def play_turn(cells):
+    def get_empty_cells(cells):
         empty_cells = []
-
         for i in range(cells):
             for j in range(cells[0]):
                 if cells[i][j].letter == ' ':
                     empty_cells.append((i, j))
+        return empty_cells
 
+    @staticmethod
+    def play_turn(cells):
         candidate_plays = Computer.check_possible_SOS(cells)
+
         if len(candidate_plays) > 0:
             return random.choice(candidate_plays)
+
+        empty_cells = Computer.get_empty_cells(cells)
 
         for pos in empty_cells:
             copy_cells = cells.copy()
             copy_cells[pos[0]][pos[1]].add_letter('S')
+
             if len(Computer.check_possible_SOS(copy_cells)) == 0:
                 candidate_plays.append(('S', pos))
+
             copy_cells = cells.copy()
             copy_cells[pos[0]][pos[1]].add_letter('O')
+
             if len(Computer.check_possible_SOS(copy_cells)) == 0:
                 candidate_plays.append(('O', pos))
 
@@ -31,6 +39,7 @@ class Computer:
         for pos in empty_cells:
             candidate_plays.append(('S', pos))
             candidate_plays.append(('O', pos))
+
         return random.choice(candidate_plays)
 
     @staticmethod
