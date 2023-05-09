bf868617c8e94c478c7c880cc3db05452be6acf0
FixerDiego <57854488+FixerDiegoB@users.noreply.github.com>
2023-05-08 20:41:49 -0500

    Update Computer.py

--- Proyecto-3S2/Sprint_4/Produccion/Computer.py
+++ Proyecto-3S2/Sprint_4/Produccion/Computer.py
@@ -1,13 +1,38 @@
+import random
+
+
 class Computer:
-    def make_turn(self, cells):
+    @staticmethod
+    def play_turn(cells):
         empty_cells = []
 
         for i in range(cells):
             for j in range(cells[0]):
-                if cells[i][j].letter == '':
+                if cells[i][j].letter == ' ':
                     empty_cells.append((i, j))
 
-        if len(empty_cells) > 0:
-            for pos in empty_cells:
-                copy_cells = cells.copy()
-                
+        candidate_plays = Computer.check_possible_SOS(cells)
+        if len(candidate_plays) > 0:
+            return random.choice(candidate_plays)
+
+        for pos in empty_cells:
+            copy_cells = cells.copy()
+            copy_cells[pos[0]][pos[1]].add_letter('S')
+            if len(Computer.check_possible_SOS(copy_cells)) == 0:
+                candidate_plays.append(('S', pos))
+            copy_cells = cells.copy()
+            copy_cells[pos[0]][pos[1]].add_letter('O')
+            if len(Computer.check_possible_SOS(copy_cells)) == 0:
+                candidate_plays.append(('O', pos))
+
+        if len(candidate_plays) > 0:
+            return random.choice(candidate_plays)
+
+        for pos in empty_cells:
+            candidate_plays.append(('S', pos))
+            candidate_plays.append(('O', pos))
+        return random.choice(candidate_plays)
+
+    @staticmethod
+    def check_possible_SOS(cells):
+        return []
